0887c3a6aaff744ed18090e2a4d238e5fcae9c5f
Jacob Traunero <jt7991@gmail.com>
2023-12-27 18:35:18 -0500

    changes

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,82 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-ui-select.nvim",
+    "nvim-lua/plenary.nvim",
+    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+    -- Only load if `make` is available. Make sure you have the system
+    -- requirements installed.
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      -- NOTE: If you are having trouble with this installation,
+      --       refer to the README for telescope-fzf-native for more instructions.
+      build = "make",
+      cond = function()
+        return vim.fn.executable("make") == 1
+      end,
+    },
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("telescope").setup({
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+        },
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({}),
+        },
+      },
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-u>"] = false,
+            ["<C-d>"] = false,
+          },
+        },
+      },
+    })
+    pcall(require("telescope").load_extension, "fzf")
+    pcall(require("telescope").load_extension, "live_grep_args")
+    pcall(require("telescope").load_extension, "ui-select")
+
+    require("telescope").load_extension("file_browser")
+    vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+    vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+    vim.keymap.set("n", "<leader>/", function()
+      -- You can pass additional configuration to telescope to change theme, layout, etc.
+      require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+        winblend = 10,
+        previewer = false,
+      }))
+    end, { desc = "[/] Fuzzily search in current buffer" })
+    vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
+    vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[F]ind [F]iles" })
+    vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "[F]ind [H]elp" })
+    vim.keymap.set("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "[F]ind current [W]ord" })
+    vim.keymap.set(
+      "n",
+      "<leader>fg",
+      require("telescope").extensions.live_grep_args.live_grep_args,
+      { desc = "[F]ind by [G]rep" }
+    )
+    vim.keymap.set("n", "<leader>fm", function()
+      require("telescope.builtin").live_grep({ additional_args = { "-U" } })
+    end, { desc = "[F]ind by [M]ultiline grep" })
+    vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "[F]ind [D]iagnostics" })
+    vim.keymap.set(
+      "n",
+      "<leader>fs",
+      require("telescope.builtin").lsp_dynamic_workspace_symbols,
+      { desc = "[F]ind [S]ymbols" }
+    )
+    vim.api.nvim_set_keymap(
+      "n",
+      "<space>fb",
+      ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+      { noremap = true }
+    )
+  end,
+}
--- lua/quickLint.lua
+++ lua/quickLint.lua
@@ -3,100 +3,108 @@ local projects = {
 	{ name = "functions", path = "~/ophelia-web/server/functions" },
 	{ name = "@shared/utils", path = "~/ophelia-web/shared/utils" },
 	{ name = "@shared/types", path = "~/ophelia-web/shared/types" },
+	{ name = "@shared/components", path = "~/ophelia-web/shared/components" },
 	{ name = "ophelia-patient", path = "~/ophelia-web/client/patient" },
+	-- { name = "data", path = "~/ophelia-web/functions/data" },
 }
 
 local function getEslintEntriesForProject(project)
-	local lint_output = vim.api.nvim_exec("!lerna run lint --scope=" .. project.name .. " -- -- --format=unix", true)
-	local lines = vim.split(lint_output, "\n")
-	local errors = {}
-	for _, line in ipairs(lines) do
-		local parts = vim.split(line, ":")
-		print(line)
-		if #parts > 1 and line:match("Error") then
-			local file = parts[1]
-			local line_number = parts[2]
-			local col = parts[3]
-			table.insert(errors, {
-				text = parts[4],
-				filename = file,
-				lnum = line_number,
-				col = col,
-			})
+	local on_text = function(_, data, _)
+		local errors = {}
+		for _, line in ipairs(data) do
+			local parts = vim.split(line, ":")
+			if #parts > 1 and line:match("Error") then
+				local file = parts[1]
+				local line_number = parts[2]
+				local col = parts[3]
+				table.insert(errors, {
+					text = parts[4],
+					filename = file,
+					lnum = line_number,
+					col = col,
+				})
+			end
 		end
+		vim.fn.setqflist(errors, "a")
 	end
-	return errors
+	print("Executing lerna run lint --scope=" .. project.name)
+	local job_id = vim.fn.jobstart("lerna run lint --scope='" .. project.name .. "' -- -- --format=unix --quiet", {
+		on_stderr = on_text,
+		on_stdout = on_text,
+		on_exit = function(_, data, _)
+			print("Finished lint for " .. project.name)
+		end,
+	})
+  return job_id
 end
 
 local function getAllEslintErrors()
-	local allEntries = {}
+  local jobs = {}
 	for _, project in ipairs(projects) do
-		local newEntries = getEslintEntriesForProject(project)
-		for _, entry in ipairs(newEntries) do
-			table.insert(allEntries, entry)
-		end
-	end
-	return allEntries
+		local job_id = getEslintEntriesForProject(project)
+    table.insert(jobs, job_id)
+  end
+  return jobs
 end
 
 local function addLintOutputToQf()
-	local allEntries = getAllEslintErrors()
-	vim.fn.setqflist(allEntries, "r")
+	local jobs = getAllEslintErrors()
+  vim.fn.jobwait(jobs, -1)
 	vim.api.nvim_exec("copen", true)
 end
 
 local function addTypeCheckOutputForProject(project)
-	local output = vim.api.nvim_exec("!lerna run typecheck --scope=" .. project.name, true)
-	local lines = vim.split(output, "\n")
-	local errors = {}
-	for _, line in ipairs(lines) do
-		local parts = vim.split(line, "%(")
-		if #parts > 1 then
-			local parts2 = vim.split(parts[2], "%)")
-			local colRowParts = vim.split(parts2[1], ",")
-			local file = parts[1]
-			local line_number = colRowParts[1]
-			local col = colRowParts[2]
-			table.insert(errors, {
-				text = parts2[2],
-				filename = vim.fn.expand(project.path) .. "/" .. file,
-				lnum = line_number,
-				col = col,
-			})
-		end
-	end
-	return errors
+	print("Executing lerna run typecheck --scope=" .. project.name)
+	local job_id = vim.fn.jobstart("lerna run typecheck --scope=" .. project.name, {
+		on_stderr = function(_, data, _)
+			local errors = {}
+			for _, line in ipairs(data) do
+				local parts = vim.split(line, "%(")
+				if #parts > 1 then
+					local parts2 = vim.split(parts[2], "%)")
+					local colRowParts = vim.split(parts2[1], ",")
+					local file = parts[1]
+					local line_number = colRowParts[1]
+					local col = colRowParts[2]
+					table.insert(errors, {
+						text = parts2[2],
+						filename = vim.fn.expand(project.path) .. "/" .. file,
+						lnum = line_number,
+						col = col,
+					})
+				end
+			end
+			vim.fn.setqflist(errors, "a")
+		end,
+		on_exit = function(_, data, _)
+			print("Finished typecheck for " .. project.name)
+		end,
+	})
+  return job_id
 end
 
 local function getAllTypeCheckErrors()
-	local allEntries = {}
+  local jobs = {}
 	for _, project in ipairs(projects) do
-		local newEntries = addTypeCheckOutputForProject(project)
-		for _, entry in ipairs(newEntries) do
-			table.insert(allEntries, entry)
-		end
+		local job_id = addTypeCheckOutputForProject(project)
+    table.insert(jobs, job_id)
 	end
-	return allEntries
+  return jobs
 end
 
 local function addTypeCheckOutputToQuickfixList()
-	local allEntries = getAllTypeCheckErrors()
-	vim.fn.setqflist(allEntries, "r")
+	local jobs = getAllTypeCheckErrors()
+
+  vim.fn.jobwait(jobs, -1)
+  -- open quickfix window once the above async jobs are done
 	vim.api.nvim_exec("copen", true)
 end
 
 local function addTypescriptDiagnosticsToQuickfix()
-	local eslintEntries = getAllEslintErrors()
-	local typeCheckEntries = getAllTypeCheckErrors()
-	local allEntries = {}
-	for _, entry in ipairs(eslintEntries) do
-		table.insert(allEntries, entry)
-	end
-
-	for _, entry in ipairs(typeCheckEntries) do
-		table.insert(allEntries, entry)
-	end
-	vim.fn.setqflist(allEntries, "r")
+	local jobs1 = getAllEslintErrors()
+  local jobs2 =	getAllTypeCheckErrors()
+  vim.fn.jobwait(jobs1, -1)
+  vim.fn.jobwait(jobs2, -1)
 	vim.api.nvim_exec("copen", true)
 end
 
