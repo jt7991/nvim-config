d27195ba11ffb3fa56ac3c117693c249ae521dc1
Jacob Traunero <jt7991@gmail.com>
2023-12-28 16:53:06 -0500

    changes

--- lua/onSave.lua
+++ lua/onSave.lua
@@ -28,7 +28,7 @@ end
 local function neoformatSetup()
 	vim.api.nvim_create_user_command("OrganizeImports", organize_imports, {})
 	vim.api.nvim_create_autocmd("BufWritePre", {
-		pattern = { "*.ts", "*.tsx" },
+		pattern = { "*.ts", "*.tsx", '*.lua', '*.js' },
 		callback = function()
 			organize_imports()
 			vim.cmd("Neoformat")
---
+++ lua/plugins/gitLinker.lua
@@ -0,0 +1,8 @@
+return {
+  'ruifm/gitlinker.nvim',
+  requires = 'nvim-lua/plenary.nvim',
+  config = function()
+    require('gitlinker').setup()
+  end,
+
+}
